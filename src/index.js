122d2b48d386164528f97c7df189329dd3f83c29
Kabakova <kabdia@mail.ru>
2024-10-11 17:35:51 +0500

    do task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const bracketsMap = {};
+  const openBrackets = [];
+  const sameBrackets = [];
+
+  bracketsConfig.forEach(([open, close]) => {
+      bracketsMap[close] = open; 
+      openBrackets.push(open); 
+      if (open === close) sameBrackets.push(open); 
+  });
+
+  const stack = []; 
+
+  for (let char of str) {
+      if (sameBrackets.includes(char)) {
+          if (stack[stack.length - 1] === char) {
+              stack.pop(); 
+          } else {
+              stack.push(char); 
+          }
+      } else if (openBrackets.includes(char)) {
+          stack.push(char); 
+      } else {
+          if (stack.length === 0 || stack.pop() !== bracketsMap[char]) {
+              return false; 
+          }
+      }
+  }
+  return stack.length === 0; 
+
 }
+
+
+//[['(', ')'], ['[', ']']]
+//'()[]'   ({[()]})  (] 
+//[{
+//]}
